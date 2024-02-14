d8646e70bee3e2c2a90336bba5d19f4fcd8e97de
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2024-02-14 11:41:28 +0530

    Add files via upload

---
+++ BoolTest.java
@@ -0,0 +1,10 @@
+class BoolTest{
+	public static void main(String[] args){
+		boolean a=false;
+		System.out.println("A is:"+a);
+		a=true;
+		if(a)
+		   System.out.println("10>9 is"+(10>9));
+		   
+	}
+}

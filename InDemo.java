15c5ab959145bbe94df3a3c0122f4f7757cffc25
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2024-02-14 12:11:52 +0530

    Add files via upload

---
+++ InDemo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class InDemo{
+	public static void main(String[] args){
+		int a,b;
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the first number:");
+		a=input.nextInt();	
+		System.out.println("Enter the Second number:");
+		b=input.nextInt();	
+		System.out.println("Sum:\n"+a+"+"+b+"="+(a+b));
+	}
+}

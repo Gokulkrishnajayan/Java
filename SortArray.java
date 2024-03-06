5eafe720d1da6a80d753e692cd303c2144bf7f37
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2024-03-06 11:07:07 +0530

    Update SortArray.java

--- SortArray.java
+++ SortArray.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Sort{
+public class SortArray{
  public static void main(String args[]){
 	int n;
 	Scanner read=new Scanner(System.in);

edd2ebba471e7f04d29e414f8b07baf43a755f6b
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2024-04-03 12:04:46 +0530

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Sort{
+ public static void main(String args[]){
+	int n;
+	Scanner read=new Scanner(System.in);
+	System.out.print("Enter the size of the array:" );
+	n=read.nextInt();
+	System.out.println("Enter the values:");
+	String arr[]=new String[n];
+	for(int i=0;i<n;++i){
+		arr[i]=read.next();
+	}
+	Arrays.sort(arr);
+	
+	for(int i=0;i<n;++i){
+		System.out.print(arr[i]+",");
+	}	
+ }
+}
+

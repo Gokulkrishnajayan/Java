716af276471e99f4699e4bd3f7156e6d857f3db0
Gokul Krishna jayan <91828907+Gokulkrishnajayan@users.noreply.github.com>
2024-04-17 09:23:50 +0530

    Add files via upload

---
+++ calcpack/calculator.java
@@ -0,0 +1,10 @@
+package calcpack;
+
+public class calculator{
+	public void add(int a,int b){
+		System.out.println("Sum ="+(a+b));
+	}
+	public void sub(int a,int b){
+		System.out.println("Difference ="+(a-b));
+	}
+}
